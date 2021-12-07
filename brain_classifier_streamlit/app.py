5d989234abb5beb0d1ec3152cf0e8398a1a9baa4
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-06 22:26:56 -0600

    Add files via upload

---
+++ brain_classifier_streamlit/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+st.title("Image Classification with Google's Teachable Machine")
+st.header("Brain Tumor MRI Classification Example")
+st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def teachable_machine_classification(img, weights_file):
+    
+    model = keras.models.load_model(weights_file)
+
+    data = np.ndarray(shape=(384, 384, 3), dtype=np.float32)
+    image = img
+    
+    size = (384, 384)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    return np.argmax(prediction)
