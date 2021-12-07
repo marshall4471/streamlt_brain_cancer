0b0535865f149aded77ed9ce341ff371186bbc01
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-07 17:09:22 -0600

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -1,13 +1,21 @@
+import keras
+from PIL import Image, ImageOps
+import numpy as np
 
-from img_classification import teachable_machine_classification
-uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded MRI.', use_column_width=True)
-        st.write("")
-        st.write("Predicting...")
-        label = teachable_machine_classification(image, 'model.h5')
-        if label == 0:
-            st.write("The MRI scan has a brain tumor")
-        else:
-            st.write("The MRI scan is healthy")
+
+def teachable_machine_classification(img, weights_file):
+    
+    model = keras.models.load_model(weights_file)
+
+    data = np.ndarray(shape=(384, 384, 3), dtype=np.float32)
+    image = img
+    
+    size = (384, 384)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    return np.argmax(prediction)
