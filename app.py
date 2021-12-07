899826811bfe711fed155d8e6df950b91f06ddde
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-07 17:44:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.header("Brain Tumor MRI Classifier")
 st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 from img_classification import teachable_machine_classification
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
-    if uploaded_file is not None:
+if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded MRI.', use_column_width=True)
         st.write("Uploaded")
