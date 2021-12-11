0df68234498ece5ccb23d02957ab32da511d4553
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 10:21:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ if uploaded_file is not None:
         st.image(image, caption='Uploaded MRI.', use_column_width=True)
         st.write("Uploaded")
         st.write("Classifying...")
-        label= prediction
+       
         label = teachable_machine_classification(image, 'model.h5')
-        if label <= 0.5:
+        if label < 0.5:
             st.write("The MRI scan detected a brain tumor")
         else:
             st.write("The MRI scan shows is healthy brain")
