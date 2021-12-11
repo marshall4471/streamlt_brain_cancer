b05597e336dea2316867a226c19a068e1fd4193e
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 12:01:15 -0600

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -1,7 +1,5 @@
 import keras
 from PIL import Image, ImageOps
-import numpy as np
-import cv2
 def teachable_machine_classification(img, prediction, weights_file):
     
     model = keras.models.load_model(weights_file)
