1b516daf461e8081a7564f0946ccb6fe723b67c9
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 10:38:24 -0600

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -6,11 +6,11 @@ def teachable_machine_classification(img, weights_file):
     
     model = keras.models.load_model(weights_file)
     image = img
-def prepare(image):
-     IMG_SIZE=384
-     img_array = cv2.imread(image, cv2.IMREAD_COLOR)
-     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-     return new_array.reshape(-1,IMG_SIZE, IMG_SIZE, 3)
+    def prepare(image):
+        IMG_SIZE=384
+        img_array = cv2.imread(image, cv2.IMREAD_COLOR)
+        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+        return new_array.reshape(-1,IMG_SIZE, IMG_SIZE, 3)
 
-     prediction=model.predict([prepare(image)])
-     return prediction
+        prediction=model.predict([prepare(image)])
+        return prediction
