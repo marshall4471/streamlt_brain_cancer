d51692b43f0dc8544d4480a8e2457a949d379a12
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 12:21:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,23 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import keras
 import cv2
-model=load_model('model.h5')
+
 model=load_model('model.h5')
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
 if uploaded_file is not None:
         image = Image.open(uploaded_file)
+        new_image=cv2.imwrite(image, image_resize)
         st.image(image, caption='Uploaded MRI.', use_column_width=True)
         st.write("Uploaded")
         st.write("Classifying...")
-        def prepare(image):
+        def prepare():
             IMG_SIZE=384
-            img_array = cv2.imread(image, cv2.IMREAD_COLOR)
+            img_array = cv2.imread(new_image, cv2.IMREAD_COLOR)
             new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
             return new_array.reshape(-1,IMG_SIZE, IMG_SIZE, 3)
      
             
-        prediction=model.predict([prepare(image)])
+        prediction=model.predict([prepare(new_image)])
                 
         label = teachable_machine_classification(image, prediction, 'model.h5')
         if prediction <= 0.5:
