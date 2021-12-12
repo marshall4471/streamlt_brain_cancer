b2a861370d2dcb8b93623c93237ce54849c5cb6d
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 23:37:27 -0600

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -3,4 +3,21 @@ from PIL import Image, ImageOps
 def teachable_machine_classification(img, prediction, weights_file):
     
     model = keras.models.load_model(weights_file)
+
+    
+    data = np.ndarray(shape=(1, 384, 384, 3), dtype=np.float32)
     image = img
+  
+    size = (384, 384)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+  
+    image_array = np.asarray(image)
+  
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+  
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    return prediction
