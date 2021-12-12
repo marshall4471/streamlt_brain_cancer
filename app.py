425f110d53509bccc7b02a4112c729f7fa5e6602
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-12 00:59:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     
     st.write("")
     label = teachable_machine_classification(image, 'model.h5')
-    if label <= 0.5:
+    if label == 0:
        st.write("The MRI scan detects a brain tumor")
     else:
        st.write("The MRI scan shows an healthy brain")
