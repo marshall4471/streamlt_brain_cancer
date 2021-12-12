9ad7dff7815639f128d61bb145c65fc117242aea
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-12-11 23:35:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,9 @@ from tensorflow.keras.preprocessing.image import load_img,img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
 import keras
-import cv2
-model=load_model('model.h5')
-def predict(image_path): 
-        image = load_img(image_path, target_size=(384, 384))
-        image = img_to_array(image)
-        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-        image = preprocess_input(image)
-        yhat = model.predict(image)
-        label = decode_predictions(yhat)
-        label = label[0][0]
-        return label 
+
      
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
